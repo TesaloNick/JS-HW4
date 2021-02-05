da624ef7c9bddec8b319cff7c870cb9378975d57
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-05 22:53:54 +0300

    Task 3

--- script.js
+++ script.js
@@ -7,10 +7,28 @@ for (let i=0; i < arr1.length; i++) {
 
 document.write('<br><br>Задание 2. Отрицательные числа, которые больше -10, но меньше -3.<br>');
 const arr2 = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
-document.write('Отрицательные числа, которые больше -10, но меньше -3: ');
+document.write('Числа: ');
 for (let i=0; i < arr2.length; i++) {
     if ((arr2[i] > -10) && (arr2[i] < -3)) {
         document.write(arr2[i] + ', ');
-
     }
 }
+
+document.write('<br><br>Задание 3. Массив со значениями от 23 до 57.<br>');
+let arr3_1 = [];
+let sum = 0;
+for (let i=23; i <= 57; i++) {
+    arr3_1[i-23] = i;
+    sum += i;
+}
+console.log('Массив с помощью For: ',  arr3_1);
+document.write('Массив с помощью For: ', arr3_1, '<br>');
+let arr3_2 = [];
+let i = 23;
+while (i <= 57) {
+    arr3_2[i-23] = i;
+    i++;
+}
+document.write('Массив с помощью While: ', arr3_2, '<br>');
+console.log('Массив с помощью While: ',  arr3_2);
+document.write('Сумма всех элементов массива: ', sum, '<br>');
