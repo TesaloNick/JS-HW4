a6e8485efaf3aa7df29ec61faf1ac7a081b7be88
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-05 22:31:17 +0300

    Task 1

---
+++ script.js
@@ -0,0 +1,7 @@
+document.write('Задание 1. Вывести элементы массива на экран<br>');
+const arr1 = [1, 2, 3, 4, 5];
+document.write('Все элементы массива: ');
+for (let i=0; i < arr1.length; i++) {
+    document.write(arr1[i] + ', ');
+}
+
